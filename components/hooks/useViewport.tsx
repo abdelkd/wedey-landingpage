e5534794e4bb42848e6f6565b8378863e8c98735
Abdelkarim Djelalda <105464717+abdelkd@users.noreply.github.com>
2025-04-04 13:56:54 +0100

    Come on

--- components/hooks/useViewport.tsx
+++ components/hooks/useViewport.tsx
@@ -7,8 +7,8 @@ type Viewport = {
     isMobile?: boolean;
 }
 
-export const useViewport = ({ width, height }: Optional<Viewport> = { width: 0, height: 0, isMobile: true }) => {
-    const [viewport, setViewport] = useState<Viewport>({ width: 0, height: 0, isMobile: true });
+export const useViewport = ({ width, height, isMobile }: Optional<Viewport> = { width: 0, height: 0, isMobile: true }) => {
+    const [viewport, setViewport] = useState<Viewport>({ width, height, isMobile });
 
     const handleResize = () => {
         setViewport({ width: window.innerWidth, height: window.innerHeight, isMobile: window.innerWidth < 400 })
